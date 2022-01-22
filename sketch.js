3860f9c0c167ed3d2e2c3c9c81de62f362dc0048
GabrielSilvadeSou <98192257+GabrielSilvadeSou@users.noreply.github.com>
2022-01-22 16:35:38 -0300

    Upload 1

---
+++ sketch.js
@@ -0,0 +1,28 @@
+var byjus;
+function setup() {
+  createCanvas(1350,600);
+  byjus=createSprite(675, 300, 200, 200)
+  byjus.shapeColor=color("white")
+}
+
+function draw() 
+{
+  background(30);
+  if(keyIsDown(UP_ARROW))  {
+    byjus.y=byjus.y-3
+  }
+  if(keyIsDown(DOWN_ARROW))  {
+    byjus.y=byjus.y+3
+  }
+  if(keyIsDown(LEFT_ARROW))  {
+     byjus.x=byjus.x-3
+  }
+  if(keyIsDown(RIGHT_ARROW))  {
+    byjus.x=byjus.x+3
+ }
+drawSprites()
+}
+
+
+
+
